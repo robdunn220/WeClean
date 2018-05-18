90a198a7ba5210f9a5e7488ea784537776c6fed8
Robert Dunn <steelersdudefour3@yahoo.com>
2018-05-18 14:50:47 -0400

    Where is this committing to?

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -12,14 +12,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Setting background color
+        view.backgroundColor = UIColor.black
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Instantiate and set actions for Sign In
+    @IBAction func SignIn(sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Sign In",
+                                                message: "",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK",
+                                                style: UIAlertActionStyle.default,
+                                                handler: nil))
+        
+        present(alertController,
+                animated: true,
+                completion: nil)
+    }
+    
+    //Instantiate and set actions for Sign Up
+    @IBAction func SignUp(sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Sign Up",
+                                                message: "su",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK",
+                                                style: UIAlertActionStyle.default,
+                                                handler: nil))
+        
+        present(alertController,
+                animated: true,
+                completion: nil)
+    }
 
 }
-
